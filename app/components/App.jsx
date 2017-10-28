5aa1cd381710e16a83fae7b710bd85abed2fe4f1
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-27 17:59:42 -0700

    add + button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
 import Notes from './Notes'
 
-export default () => <Notes />;
+export default () => (
+  <div>
+    <Notes />
+    <button onClick={() => console.log('add note')}> + </button>
+  </div>
+)
