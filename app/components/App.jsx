22624ad7bb77ea276e80a03a11ecf2f22ec7aecc
Xilin Sun <s.sn.giraffe@gmail.com>
2017-10-31 23:30:38 -0700

    update app: prepare for + button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import uuid from 'uuid'
 import Notes from './Notes'
+
+const notes = [
+  {
+    id: uuid.v4(),
+    task: 'Learn Javascript'
+  },
+  {
+    id: uuid.v4(),
+    task: 'Learn more Javascript'
+  }
+]
 
 export default () => (
   <div>
-    <Notes />
+    <Notes notes={notes} />
     <button onClick={() => console.log('add note')}> + </button>
   </div>
 )
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,24 +1,7 @@
 import React from 'react'
-import uuid from 'uuid'
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Javascript'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn more Javascript'
-  }
-]
-
-export default () => {
-  console.log(notes)
-  return (
-    <ul>{
-      notes.map(
-        note => <li key={note.id}>{note.task}</li> 
-      )
-    }</ul>
-  )
-}
+export default ({notes}) => (
+  <ul> {notes.map(note => 
+    <li key={note.id}> {note.task}</li>
+  )} </ul>
+)
