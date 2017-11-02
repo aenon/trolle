271ca89f3282853accd87edd6c6e01a417eba9a6
Xilin Sun <s.sn.giraffe@gmail.com>
2017-11-02 00:00:17 -0700

    add onDelete function

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -3,6 +3,6 @@ import React from 'react'
 export default ({task, onDelete}) => (
   <div>
     <span>{task}</span>
-    <button onClick={() => console.log('delete note' + task)}> x </button>
+    <button onClick={onDelete}> x </button>
   </div>
 )
